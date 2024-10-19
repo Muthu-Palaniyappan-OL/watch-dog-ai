75e1a2e11706b99fbb218736df19202e04b9cfd7
balajianbalagan <vijibalaji2003@gmail.com>
2024-10-19 10:45:46 +0530

    redirect to dashboard and sign up modified

--- watch-dog-frontend/src/LandingPage/LandingPage.tsx
+++ watch-dog-frontend/src/LandingPage/LandingPage.tsx
@@ -76,8 +76,9 @@ const LandingPage = () => {
               <a href="#" className="text-lg font-semibold leading-6 text-stone-300  hover:text-sky-300">Company</a>
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center space-x-2">
+            <a href="https://github.com/Muthu-Palaniyappan-OL/watch-dog-ai/" className="text-lg font-semibold leading-10 text-stone-300">
               <span className="text-lg font-semibold leading-10 text-stone-300" aria-hidden="true">Check code</span>
-
+</a>
               <a href="https://github.com/Muthu-Palaniyappan-OL/watch-dog-ai/" className="text-lg font-semibold leading-10 text-stone-300">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
