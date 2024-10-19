e2b472de8968024259f627ac591338b036737314
Muthu Palaniyappan OL <muthu892542@gmail.com>
2024-10-19 23:20:00 +0530

    jayenth push

--- watch-dog-backend/vision.py
+++ watch-dog-backend/vision.py
@@ -2,16 +2,27 @@ import requests
 import base64
 from dotenv import load_dotenv
 import json
+import cv2
+from flask import current_app
+from db import TranscriptDetailed, db, app
 import os
 
 
-def get_caption(image_path):
+def image_path_to_image_b64(image_path):
+    with open(image_path, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+
+
+def ndarray_to_image_b64(image_arr):
+    _, buffer = cv2.imencode(".jpg", image_arr, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+    return base64.b64encode(buffer).decode("utf-8")
+
+
+async def get_caption(image_b64, frame_number, camera_id):
     invoke_url = "https://ai.api.nvidia.com/v1/gr/meta/llama-3.2-90b-vision-instruct/chat/completions"
     stream = False
     api_token = os.getenv("NVIDIA_API_TOKEN")
     # Read the image and encode it to base64
-    with open(image_path, "rb") as f:
-        image_b64 = base64.b64encode(f.read()).decode()
 
     assert (
         len(image_b64) < 180_000
@@ -31,7 +42,7 @@ def get_caption(image_path):
             }
         ],
         "max_tokens": 512,
-        "temperature": 0.5,
+        "temperature": 0.4,
         "top_p": 1.00,
         "stream": stream,
     }
@@ -55,6 +66,29 @@ def get_caption(image_path):
         try:
             # Convert string content to dictionary
             cleaned_output = json.loads(content)
+
+            with app.app_context():
+                try:
+                    # {"unusual_activity": "none", "human_activity": "People walking on the sidewalk, some carrying bags or backpacks. A few individuals are standing near the street, possibly waiting for something or someone.", "animal_activity": "none", "time": "day", "unusual_crowd": "none", "lighting_conditions": "well-lit", "vehicle_details": "none", "number_of_individuals": "approximately 10-15", "object_presence": "none", "context_notes": "The scene appears to be a typical urban setting with people going about their daily business. There are no visible signs of distress or unusual behavior among the individuals present."}
+                    data = TranscriptDetailed()
+                    data.camera_id = camera_id
+                    data.frame_number = frame_number
+                    data.unusual_activity = cleaned_output["unusual_activity"]
+                    data.human_activity = cleaned_output["human_activity"]
+                    data.animal_activity = cleaned_output["animal_activity"]
+                    data.time = cleaned_output["time"]
+                    data.unusual_crowd = cleaned_output["unusual_crowd"]
+                    data.lighting_conditions = cleaned_output["lighting_conditions"]
+                    data.vehicle_details = cleaned_output["vehicle_details"]
+                    data.number_of_individuals = cleaned_output["number_of_individuals"]
+                    data.object_presence = cleaned_output["object_presence"]
+                    data.context_notes = cleaned_output["context_notes"]
+                    db.session.add(data)
+                    db.session.commit()
+                except Exception as e:
+                    db.session.rollback()  # Rollback the session in case of error
+                    print(f"Error: {e}")
+
             return cleaned_output
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
