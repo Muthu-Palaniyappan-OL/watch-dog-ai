09ef8ec7db6eb79cbae3936d03f13a9766f65dbc
balajianbalagan <vijibalaji2003@gmail.com>
2024-10-19 11:39:24 +0530

    align image

--- watch-dog-frontend/src/LandingPage/LandingPage.tsx
+++ watch-dog-frontend/src/LandingPage/LandingPage.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import SignUpModal from './SignUpModal';
 import { LockClosedIcon, VideoCameraIcon, BellAlertIcon, DocumentTextIcon, ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline'
 import { motion } from 'framer-motion';
+
+
 const features = [
   {
     name: 'Natural Language Queries',
@@ -46,7 +48,16 @@ const LandingPage = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+  const [isOpen, setIsOpen] = useState(false);
 
+  const openMenu = () => {
+    setIsOpen(true);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+  const constraintsRef = useRef(null)
   return (
 
     <div>
@@ -62,7 +73,7 @@ const LandingPage = () => {
             </div>
 
             <div className="flex lg:hidden">
-              <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-stone-300">
+              <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-stone-300" onClick={openMenu}>
                 <span className="sr-only">Open main menu</span>
                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -87,36 +98,53 @@ const LandingPage = () => {
             </div>
 
           </nav>
-          <div className="lg:hidden" role="dialog" aria-modal="true">
-            <div className="fixed inset-0 z-50"></div>
-            <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Your Company</span>
-                  <img className="h-20 w-auto" src="./watchdog.svg" alt="" />
-                </a>
-                <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
-                  <span className="sr-only">Close menu</span>
-                  <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                  </svg>
-                </button>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Product</a>
-                    <a href="#Features" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
-                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
-                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
-                  </div>
-                  <div className="py-6">
-                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
-                  </div>
+          {isOpen && (
+        <div className="lg:hidden" role="dialog" aria-modal="true">
+          <div className="fixed inset-0 z-50 bg-black bg-opacity-50" onClick={closeMenu}></div>
+          <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <img className="h-20 w-auto" src="./watchdog.svg" alt="Logo" />
+              </a>
+              <button type="button" onClick={closeMenu} className="-m-2.5 rounded-md p-2.5 text-gray-700">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                    Product
+                  </a>
+                  <a href="#Features" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                    Features
+                  </a>
+                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                    Marketplace
+                  </a>
+                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                    Company
+                  </a>
+                </div>
+                <div className="py-6">
+                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                    Log in
+                  </a>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      )}
         </header>
         {isModalOpen && <SignUpModal onClose={closeModal} />}
         <div className="relative isolate px-6 pt-14 lg:px-8">
@@ -130,31 +158,34 @@ const LandingPage = () => {
           </div>
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="text-center">
-              <motion.img
-                className="absolute left-[408px] top-[140px]"
-                drag
-                src='/doggo.png'
-                dragSnapToOrigin
-                dragTransition={{ bounceStiffness: 40, bounceDamping: 7 }}
-                animate={["initial"]}
-                variants={{
-                  initial: {
-                    y: [-10, 10],
-                    x: [-5, 5],
-                    rotate: 5,
-                    transition: {
-                      delay: 1,
-                      duration: 2,
-                      repeat: Infinity,
-                      // repeatDelay: 0.2,
-                      repeatType: "reverse"
-                    }
-                  }
-                }}
-              >
-              </motion.img>
+            <motion.div ref={constraintsRef}>
+            <motion.img
+      className="absolute left-1/20 top-1/4 transform -translate-x-1/2 sm:w-48 w-32 lg:w-64 z-0"
+      drag
+      src="/doggo.png"
+      dragElastic={0} // No overshoot, snaps directly back
+      dragConstraints={{ left: -300, right: -10, top:-100, bottom:100 }}
+      dragTransition={{ bounceStiffness: 40, bounceDamping: 7 }}
+      animate={["initial"]}
+      variants={{
+        initial: {
+          y: [0, 10],
+          x: [-90, -80],
+          rotate: 5,
+          transition: {
+            delay: 1,
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+        },
+      }}
+    />
+              </motion.div>
+           
 
-              <h1 className="text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Revolutionize Your Security with WatchDog AI</h1>
+             <div className='relative z-10'>
+             <h1 className="z-10 text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Revolutionize Your Security with WatchDog AI</h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">The Future of Surveillance: AI That Understands Your World</p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <div>
@@ -172,6 +203,7 @@ const LandingPage = () => {
 
                 </div> <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
               </div>
+             </div>
             </div>
           </div>
           <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
