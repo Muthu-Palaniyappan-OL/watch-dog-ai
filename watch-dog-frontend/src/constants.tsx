ddd58088dbcaa99c2c652b805a43655f16f7033e
AstroBoy73 <42893328+balajianbalagan@users.noreply.github.com>
2024-10-20 18:36:09 +0530

    Balaji branch (#13)
    * vision api added
    
    * vision api added
    
    * vision api added
    
    * how it works added and learn more to canva ppt
    
    * analytics added
    
    * transcript fetching
    
    * fetch all transcripts
    
    * edit cam
    
    * add cam
    
    * minor change constant

--- watch-dog-frontend/src/constants.tsx
+++ watch-dog-frontend/src/constants.tsx
@@ -1,6 +1,6 @@
 // src/constants.ts
 export const CAMS_API_URL = 'http://127.0.0.1:5000/getcams';
-export const ANALYTICS_API_URL = 'http://127.0.1:5000/analytics/';
+export const ANALYTICS_API_URL = 'http://127.0.0.1:5000/analytics/';
 export const TRANSCRIPTS_API_URL = 'http://127.0.0.1:5000/transcripts/';
 export const CHAT_API_URL = 'http://127.0.0.1:5200/chat/';
 export const LOCAL_CHAT_API_URL = 'http://127.0.0.1:5000/chat/';
