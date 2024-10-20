f6d369176b1a8f3d1c7d0fbc852b35d697b26443
Muthu Palaniyappan OL <muthu892542@gmail.com>
2024-10-20 15:58:06 +0530

    flask service

--- watch-dog-backend/run.sh
+++ watch-dog-backend/run.sh
@@ -5,4 +5,5 @@ cd ./watch-dog-ai/watch-dog-backend/
 python3 -m venv venv
 /home/ec2-user/watch-dog-ai/watch-dog-backend/venv/bin/python3 -m pip install --upgrade pip
 source venv/bin/activate
-pip install -r requirements.txt
+mkdir ~/pip_temp
+TMPDIR=~/pip_temp pip install -r requirements.txt
